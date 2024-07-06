54f3e7f49b1dfab60ec093fc0855a371528bf1fb
jon94 <jonathan.lim@datadoghq.com>
2024-07-06 15:38:45 +0800

    added cloudrunpythonbe endpoint after creating lb

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.get("/status/:code", (req, res) => {
 
 app.get("/attack", (req, res) => {
   axios
-    .get("http://13.235.255.1:5500/api/getRequest")
+    .get("https://cloudrunpythonbe-n2at3rsn5a-uc.a.run.app/api/getRequest")
     .then((response) => {
       res.status(200).send(response.data);
     })
