0465a26cc6368e0ffec52d307c16fb920320f71a
Kenneth Foo <101826160+kennethfoo24@users.noreply.github.com>
2024-07-08 12:09:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { datadogRum } from '@datadog/browser-rum';
 const express = require("express");
 const path = require("path");
 const logger = require("./logger");
@@ -6,6 +7,25 @@ const port = 3000;
 const tracer = require("dd-trace").init();
 const span = tracer.scope().active();
 const axios = require("axios").default;
+
+datadogRum.init({
+    applicationId: 'abf6318d-6424-4d8d-9f8d-43e4e8e498ce',
+    clientToken: 'pub815b4dbf3e6fc56dee01fe5345bd8e6a',
+    // `site` refers to the Datadog site parameter of your organization
+    // see https://docs.datadoghq.com/getting_started/site/
+    site: 'datadoghq.com',
+    service: 'avengers-app-browser',
+    env: 'avengers-app',
+    // Specify a version number to identify the deployed version of your application in Datadog
+    version: 'phase:1', 
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 100,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+    allowedTracingUrls: [(url) => url.startsWith("http://kenneth-marvel"), (url) => url.startsWith("https://kenneth-marvel")],
+});
 
 // Function to simulate errors with stack trace
 const simulateError = (message) => {
