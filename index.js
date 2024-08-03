6432ac796427dff031a90418567a4201a202bda9
Kenneth Foo <101826160+kennethfoo24@users.noreply.github.com>
2024-08-04 01:50:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ require('dotenv').config(); // Using dotenv for environment variables
 const port = process.env.PORT || 3000;
 
 // Database connection configuration using environment variables
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  max: 20, // Maximum number of connections in the pool
-  idleTimeoutMillis: 5000, // Close idle clients after 5 seconds
-  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   max: 20, // Maximum number of connections in the pool
+//   idleTimeoutMillis: 5000, // Close idle clients after 5 seconds
+//   connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
