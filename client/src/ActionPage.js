f37ed8c93bcce8d3a3628c1268de240f48824ffd
wipawan <mew.amornchairattanakul@datadoghq.com>
2024-08-16 16:22:00 +0800

    update mobile first view

--- client/src/ActionPage.js
+++ client/src/ActionPage.js
@@ -1,52 +1,77 @@
-import React, { useState } from "react";
-import { Menu } from "antd";
+import React, { useRef, useState } from "react";
+import { Tabs, Swiper } from "antd-mobile";
 import SelectCharacter from "./components/SelectCharacter";
 import SimulateResponses from "./components/SimulateResponses";
 import SQLInjection from "./components/SQLInjection";
+import "./index.css";
 
-const items = [
+const tabItems = [
   {
-    label: "Select Character",
+    label: "Character",
     key: "selectCharacter",
   },
   {
-    label: "Simulate HTTP Reponses",
+    label: "HTTP",
     key: "simulateHttpResponses",
   },
   {
-    label: "SQL Injection",
+    label: "Security",
     key: "injectSQL",
   },
 ];
 
 const ActionPage = () => {
-  const [current, setCurrent] = useState(items[0].key);
-  const onClick = (e) => {
-    setCurrent(e.key);
-  };
-
-  const renderContent = () => {
-    switch (current) {
-      case "selectCharacter":
-        return <SelectCharacter />;
-      case "simulateHttpResponses":
-        return <SimulateResponses />;
-      case "injectSQL":
-        return <SQLInjection />;
-      default:
-        return <></>;
-    }
-  };
+  const swiperRef = useRef(null);
+  const [activeIndex, setActiveIndex] = useState(1);
 
   return (
     <div>
-      <Menu
-        onClick={onClick}
-        selectedKeys={[current]}
-        mode="horizontal"
-        items={items}
-      />
-      <div className="container">{renderContent()}</div>
+      <Tabs
+        activeKey={tabItems[activeIndex].key}
+        onChange={(key) => {
+          const index = tabItems.findIndex((item) => item.key === key);
+          setActiveIndex(index);
+          swiperRef.current?.swipeTo(index);
+        }}
+      >
+        {tabItems.map((item) => (
+          <Tabs.Tab
+            style={{
+              "--title-font-size": "30px",
+              "--active-title-color": "white",
+              color: "white",
+            }}
+            title={item.label}
+            key={item.key}
+          />
+        ))}
+      </Tabs>
+      <Swiper
+        direction="horizontal"
+        loop
+        indicator={() => null}
+        ref={swiperRef}
+        defaultIndex={activeIndex}
+        onIndexChange={(index) => {
+          setActiveIndex(index);
+        }}
+      >
+        <Swiper.Item>
+          <div className="container">
+            <SelectCharacter />
+          </div>
+        </Swiper.Item>
+        <Swiper.Item>
+          <div className="container">
+            <SimulateResponses />
+          </div>
+        </Swiper.Item>
+        <Swiper.Item>
+          <div className="container">
+            <SQLInjection />
+          </div>
+        </Swiper.Item>
+      </Swiper>
     </div>
   );
 };
